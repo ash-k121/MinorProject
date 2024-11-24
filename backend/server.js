ee4dd57c8bda2cf236cbb316b76a568176070ab8
Aryan Goyal <aryangoyal90@outlook.com>
2024-11-24 18:32:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT;
 
 app.use(cors(
   {
-    origin: 'http://localhost:5173',
+    origin: 'https://project-social-media.onrender.com/',
   }
 ))
 app.use(cors()); // Enable CORS for cross-origin requests
